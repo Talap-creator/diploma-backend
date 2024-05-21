a48da4f900de87894252ad1a4539791df7f0c140
Akbota <aqbotamaratqyzy@gmail.com>
2024-05-21 22:18:24 +0500

    feat: csrf

--- ecoMarketAPI/urls.py
+++ ecoMarketAPI/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OrderCreateAPIView, OrderListAPIView, ProductCategoryList, ProductList, delete_cookie, add_to_cart, get_cart, RegisterAPIView, LoginAPIView, ProductDetailView, ReviewView, ReviewDetailView
+from .views import OrderCreateAPIView, OrderListAPIView, ProductCategoryList, ProductList, delete_cookie, add_to_cart, get_cart, RegisterAPIView, LoginAPIView, ProductDetailView, ReviewView, ReviewDetailView, get_csrf, ProductByCategoryViewSet, ReviewListView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
@@ -9,9 +9,12 @@ urlpatterns = [
     path("product-category-list/", ProductCategoryList.as_view()),
     path("product-list/", ProductList.as_view()),
     path("product-list/<int:pk>/add-to-cart", add_to_cart),
+    path("product-list/<int:id>/review-list", ReviewListView.as_view()),
     path("product-list/<int:id>/add-review", ReviewView.as_view()),
     path("product-list/<int:id>/review/<int:review_id>", ReviewDetailView.as_view()),
     path("product-list/<int:id>", ProductDetailView.as_view()),
     path("cart/",get_cart),
-    path("cart/delete/<int:product_id>", delete_cookie)
+    path("cart/delete/<int:product_id>", delete_cookie),
+    path("product-by-category/<int:category_id>", ProductByCategoryViewSet.as_view({'get': 'list'})),
+    path("csrf/", get_csrf)
 ]
